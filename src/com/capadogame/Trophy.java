8ba571ffbc939f840c7e7d10ab9dbe656afc2d9e
auliaaepa <76677130+auliaaepa@users.noreply.github.com>
2021-01-19 09:44:12 +0700

    Update Trophy.java

--- src/com/capadogame/Trophy.java
+++ src/com/capadogame/Trophy.java
@@ -30,42 +30,42 @@ public class Trophy {
 	
 	public void loadTrophy() {
 		try {
-    		File f = new File(dataPath, fileNameTrophy);
-    		if(!f.isFile()) {
-    			createTrophy();
-    		}
-    		BufferedReader reader = new BufferedReader(new InputStreamReader (new FileInputStream(f)));
-    		setTrophy(Integer.parseInt(reader.readLine()));
-    		reader.close();
-    	}
-    	catch(Exception e) { }
+			File f = new File(dataPath, fileNameTrophy);
+			if(!f.isFile()) {
+				createTrophy();
+			}
+			BufferedReader reader = new BufferedReader(new InputStreamReader (new FileInputStream(f)));
+			setTrophy(Integer.parseInt(reader.readLine()));
+			reader.close();
+		}
+		catch(Exception e) { }
 	}
 	
 	public void createTrophy() {
 		try {
-    		File file = new File(dataPath, fileNameTrophy);
-    		
-    		FileWriter output = new FileWriter(file);
-    		BufferedWriter writer = new BufferedWriter(output);
-    		writer.write("0");
-    		
-    		writer.close();
-    	}
-    	catch(Exception e) { }
+			File file = new File(dataPath, fileNameTrophy);
+
+			FileWriter output = new FileWriter(file);
+			BufferedWriter writer = new BufferedWriter(output);
+			writer.write("0");
+
+			writer.close();
+		}
+		catch(Exception e) { }
 	}
 	
 	public void updateTrophy(int trophy) {
 		FileWriter output = null;
-    	try {
-    		File f = new File(dataPath, fileNameTrophy);
-    		output = new FileWriter(f);
-    		BufferedWriter writer = new BufferedWriter(output); 
-    		
-    		writer.write(String.valueOf(trophy));
-    		
-    		writer.close();
-    	}
-    	catch(Exception e) { }
+		try {
+			File f = new File(dataPath, fileNameTrophy);
+			output = new FileWriter(f);
+			BufferedWriter writer = new BufferedWriter(output); 
+
+			writer.write(String.valueOf(trophy));
+
+			writer.close();
+		}
+		catch(Exception e) { }
 	}
 	
 }
