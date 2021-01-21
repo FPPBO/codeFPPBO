40256ea663f6b107fb1529d18a539d9f2c860c9c
auliaaepa <76677130+auliaaepa@users.noreply.github.com>
2021-01-21 15:54:55 +0700

    Update MediumPanel.java

--- src/com/capadogame/MediumPanel.java
+++ src/com/capadogame/MediumPanel.java
@@ -31,23 +31,24 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MediumPanel extends JPanel {
+	private final int LEVEL = 3;
 	private int areaWidth, areaHeight;
 	private Trophy trophy;
 	private Heart heart;
 	private JLabel jlSelectLevel;
 	private int[] targetMedium;
 	private Boolean[] clearMedium;
-	private ImageIcon iiSetting, iiBack, iiStart, iiPprofil, iiShop;
+	private ImageIcon iiSetting, iiBack, iiStart, iiProfil, iiShop;
 	private List<ImageIcon> iiMedium;
 	private BufferedImage biTrophy, biHeart;
-	private JButton jbSetting, jbBack, jbStart, jbPprofil, jbShop;
+	private JButton jbSetting, jbBack, jbStart, jbProfil, jbShop;
 	private List<JButton> jbMedium;
 	private JPanel topBar, centerBox, titleBox, selectBox, bottomBar;
 	private BoxLayout boxLayoutTB, boxLayoutBB;
 	private FlowLayout flowLayoutSB;
 	private BorderLayout borderLayoutP, borderLayoutCB, borderLayoutTLB;
 	private String dataPath;
-	private String fileNameClearMedium = "dataClearMedium";
+	private String fileNameClearMedium;
 	
 	public MediumPanel (CardLayoutWindow parent, int areaWidth, int areaHeight,
 			Trophy trophy, Heart heart) {
@@ -111,15 +112,19 @@ public class MediumPanel extends JPanel {
 		flowLayoutSB = new FlowLayout(FlowLayout.LEFT, 10, 10);
 		selectBox.setLayout(flowLayoutSB);
 		
-		targetMedium = new int[3];
+		targetMedium = new int[LEVEL];
 		targetMedium[0] = 5;
 		targetMedium[1] = 10;
 		targetMedium[2] = 15;
-		clearMedium = new Boolean[3];
-		loadClearMedium();
+		
+		clearMedium = new Boolean[LEVEL];
+		for (int i=0; i<clearMedium.length; i++) {
+			clearMedium[i] = false;
+		}
+		
 		jbMedium = new ArrayList<JButton>();
 		iiMedium = new ArrayList<ImageIcon>();
-		for (int i=0; i<3; i++) {
+		for (int i=0; i<LEVEL; i++) {
 			jbMedium.add(new JButton());
 			jbMedium.get(i).setBorder(BorderFactory.createCompoundBorder(
 					BorderFactory.createRaisedSoftBevelBorder(), 
@@ -130,13 +135,9 @@ public class MediumPanel extends JPanel {
 			jbMedium.get(i).setBackground(new Color(250, 220, 90));
 			jbMedium.get(i).setActionCommand("Medium" + (i+1));
 			
-			if (clearMedium[i] == true) {
-				setClearButton(i);
-			} else {
-				iiMedium.add(new ImageIcon(new ImageIcon("res/fmedium-" + (i+1) + ".png").getImage().
-						getScaledInstance(areaHeight/8, areaHeight/8, Image.SCALE_SMOOTH)));
-				jbMedium.get(i).setIcon(iiMedium.get(i));
-			}
+			iiMedium.add(new ImageIcon(new ImageIcon("res/fmedium-" + (i+1) + ".png").getImage().
+					getScaledInstance(areaHeight/8, areaHeight/8, Image.SCALE_SMOOTH)));
+			jbMedium.get(i).setIcon(iiMedium.get(i));
 		}
 				
 		for (JButton jbMedium: jbMedium) {
@@ -163,13 +164,13 @@ public class MediumPanel extends JPanel {
 				getScaledInstance(areaHeight/9, areaHeight/9, Image.SCALE_SMOOTH));
 		jbStart.setIcon(iiStart);
 		
-		jbPprofil = new JButton();		
-		jbPprofil.setBorder(null);
-		jbPprofil.setFocusPainted(false);
-		jbPprofil.setActionCommand("Pprofil");
-		iiPprofil = new ImageIcon(new ImageIcon("res/non-profil.png").getImage().
+		jbProfil = new JButton();		
+		jbProfil.setBorder(null);
+		jbProfil.setFocusPainted(false);
+		jbProfil.setActionCommand("Profil");
+		iiProfil = new ImageIcon(new ImageIcon("res/non-profil.png").getImage().
 				getScaledInstance(areaHeight/9, areaHeight/9, Image.SCALE_SMOOTH));
-		jbPprofil.setIcon(iiPprofil);
+		jbProfil.setIcon(iiProfil);
 		
 		jbShop = new JButton();
 		jbShop.setBorder(null);
@@ -182,7 +183,7 @@ public class MediumPanel extends JPanel {
 		bottomBar.add(Box.createRigidArea(new Dimension ((areaWidth-3*(areaHeight/9))/4, 0)));
 		bottomBar.add(jbStart);
 		bottomBar.add(Box.createRigidArea(new Dimension ((areaWidth-3*(areaHeight/9))/4, 0)));
-		bottomBar.add(jbPprofil);
+		bottomBar.add(jbProfil);
 		bottomBar.add(Box.createRigidArea(new Dimension ((areaWidth-3*(areaHeight/9))/4, 0)));
 		bottomBar.add(jbShop);
 		bottomBar.setBackground(Color.WHITE);
@@ -200,7 +201,7 @@ public class MediumPanel extends JPanel {
 			jbMedium.addActionListener(parent);
 		}
 		jbStart.addActionListener(parent);
-		jbPprofil.addActionListener(parent);
+		jbProfil.addActionListener(parent);
 		jbShop.addActionListener(parent);
 	}
 	
@@ -212,14 +213,20 @@ public class MediumPanel extends JPanel {
 		return clearMedium[index];
 	}
 	
-	public void setClearMedium(int index, Boolean clear) {
-		clearMedium[index] = clear;
+	public void setClearMedium(int index) {
+		clearMedium[index] = true;
+		setClearButton(index);
 	}
 	
 	public void setClearButton(int index) {
 		iiMedium.add(index, new ImageIcon(new ImageIcon("res/cmedium-" + (index+1) + ".png").getImage().
 				getScaledInstance(areaHeight/8, areaHeight/8, Image.SCALE_SMOOTH)));
 		jbMedium.get(index).setIcon(iiMedium.get(index));
+	}
+	
+	public void setfileNameClearMedium(String username) {
+		fileNameClearMedium = "dataClearMedium" + username;
+		loadClearMedium();
 	}
 	
 	public void loadClearMedium() {
@@ -230,7 +237,8 @@ public class MediumPanel extends JPanel {
 			}
 			BufferedReader reader = new BufferedReader(new InputStreamReader (new FileInputStream(f)));
 			for (int i=0; i<clearMedium.length; i++) {
-				clearMedium[i] = Boolean.parseBoolean(reader.readLine());
+				if (Boolean.parseBoolean(reader.readLine()))
+					setClearMedium(i);
 			}
 			reader.close();
 		}
@@ -275,7 +283,7 @@ public class MediumPanel extends JPanel {
 		
 		drawTrophy(g);
 		drawHeart(g);
-	    }
+	}
 	
 	public void drawTrophy(Graphics g) {
 		g.setColor(new Color(245, 222, 89));
@@ -315,14 +323,14 @@ public class MediumPanel extends JPanel {
 	}
 	
 	public BufferedImage resize(BufferedImage img, int newW, int newH) { 
-	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
-	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+		Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+		BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
 
-	    Graphics2D g2d = dimg.createGraphics();
-	    g2d.drawImage(tmp, 0, 0, null);
-	    g2d.dispose();
+		Graphics2D g2d = dimg.createGraphics();
+		g2d.drawImage(tmp, 0, 0, null);
+		g2d.dispose();
 
-	    return dimg;
+		return dimg;
 	} 
 	
 }
