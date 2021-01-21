bf519a7e621ef844a7a616826041cd77629681a8
auliaaepa <76677130+auliaaepa@users.noreply.github.com>
2021-01-21 23:02:19 +0700

    Update ShopPanel.java

--- src/com/capadogame/ShopPanel.java
+++ src/com/capadogame/ShopPanel.java
@@ -1,91 +1,38 @@
 package com.capadogame;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.imageio.ImageIO;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class ShopPanel extends JPanel implements ActionListener {
 	private int BIRDSHOPLIMIT = 5;
-	private int areaWidth, areaHeight;
-	private Trophy trophy;
-	private Heart heart;
 	private Bird bird;
 	private Boolean[] birdPurchase;
 	private JLabel jlBird;
-	private ImageIcon iiSetting, iiStart, iiProfil, iiShop;
-	private BufferedImage biTrophy, biHeart;
-	private JButton jbSetting, jbStart, jbProfil,jbShop;
-	private JPanel topBar, centerBox, bottomBar;
-	private BoxLayout boxLayoutTB, boxLayoutCB, boxLayoutBB;
+	private TopBar topBar;
+	private JPanel centerBox, birdTitle, birdArea;
+	private List<JPanel> birdShop;
+	private BottomBar bottomBar;
+	private BoxLayout boxLayoutCB;
+	private FlowLayout flowLayoutBT;
 	private BorderLayout borderLayoutP;
 	private CardLayout cardManagerBird;
-	private JPanel birdTitle;
-	private FlowLayout flowLayoutBT;
-	private JPanel birdArea;
-	private List<JPanel> birdShop;
 	private String dataPath;
 	private String fileNameBirdPurchase;
 	
 	public ShopPanel (CardLayoutWindow parent, int areaWidth, int areaHeight, 
 			Trophy trophy, Heart heart, Bird bird) {
-		this.areaWidth = areaWidth;
-		this.areaHeight = areaHeight;
-		this.trophy = trophy;
-		this.heart = heart;
 		this.bird = bird;
 		try {
 			dataPath = ShopPanel.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
 		} catch (URISyntaxException e) {}
 		
 		/* Top Bar */
-		topBar = new JPanel();
-		boxLayoutTB = new BoxLayout(topBar, BoxLayout.X_AXIS);
-		topBar.setLayout(boxLayoutTB);
-		
-		jbSetting = new JButton();
-		jbSetting.setBorder(null);
-		jbSetting.setFocusPainted(false);
-		jbSetting.setActionCommand("Setting");
-		iiSetting = new ImageIcon(new ImageIcon("res/setting.png").getImage().
-				getScaledInstance(areaHeight/12, areaHeight/12, Image.SCALE_SMOOTH));
-		jbSetting.setIcon(iiSetting);
-		
-		try {
-			biTrophy = ImageIO.read(new File("C:/Users/Hp/git/CapadoGame/res/trophy.png"));
-			biHeart = ImageIO.read(new File("C:/Users/Hp/git/CapadoGame/res/heart.png"));
-		} catch (IOException e) {}
-		
-		topBar.add(Box.createRigidArea(new Dimension(((areaWidth-3*(areaHeight/9))/4 - areaHeight/12)/2, 0)));
-		topBar.add(jbSetting);
-		topBar.setOpaque(false);
+		topBar = new TopBar(parent, areaWidth, areaHeight, trophy, heart);
 		
 		 /* Center Box */
 		centerBox = new JPanel();
@@ -135,41 +82,7 @@ public class ShopPanel extends JPanel implements ActionListener {
 		centerBox.setBackground(new Color(185, 226, 245));
 		
 		/* Bottom Bar */
-		bottomBar = new JPanel();
-		boxLayoutBB = new BoxLayout(bottomBar, BoxLayout.X_AXIS);
-		bottomBar.setLayout(boxLayoutBB);
-		
-		jbStart = new JButton();
-		jbStart.setBorder(null);
-		jbStart.setFocusPainted(false);
-		jbStart.setActionCommand("Start");
-		iiStart = new ImageIcon(new ImageIcon("res/non-start.png").getImage().
-				getScaledInstance(areaHeight/9, areaHeight/9, Image.SCALE_SMOOTH));
-		jbStart.setIcon(iiStart);
-		
-		jbProfil = new JButton();		
-		jbProfil.setBorder(null);
-		jbProfil.setFocusPainted(false);
-		jbProfil.setActionCommand("Profil");
-		iiProfil = new ImageIcon(new ImageIcon("res/non-profil.png").getImage().
-				getScaledInstance(areaHeight/9, areaHeight/9, Image.SCALE_SMOOTH));
-		jbProfil.setIcon(iiProfil);
-		
-		jbShop = new JButton();
-		jbShop.setBorder(null);
-		jbShop.setFocusPainted(false);
-		jbShop.setActionCommand("Shop");
-		iiShop = new ImageIcon(new ImageIcon("res/shop.png").getImage().
-				getScaledInstance(areaHeight/9, areaHeight/9, Image.SCALE_SMOOTH));
-		jbShop.setIcon(iiShop);
-		
-		bottomBar.add(Box.createRigidArea(new Dimension ((areaWidth-3*(areaHeight/9))/4, 0)));
-		bottomBar.add(jbStart);
-		bottomBar.add(Box.createRigidArea(new Dimension ((areaWidth-3*(areaHeight/9))/4, 0)));
-		bottomBar.add(jbProfil);
-		bottomBar.add(Box.createRigidArea(new Dimension ((areaWidth-3*(areaHeight/9))/4, 0)));
-		bottomBar.add(jbShop);
-		bottomBar.setBackground(Color.WHITE);
+		bottomBar = new BottomBar(parent, areaWidth, areaHeight, 2);
 		
 		/* Panel */		
 		borderLayoutP = new BorderLayout();
@@ -180,11 +93,6 @@ public class ShopPanel extends JPanel implements ActionListener {
 		add(Box.createRigidArea(new Dimension (15, 0)), BorderLayout.LINE_END);
 		add(bottomBar, BorderLayout.SOUTH);
 		setBackground(new Color(185, 226, 245));
-		
-		jbSetting.addActionListener(parent);
-		jbStart.addActionListener(parent);
-		jbProfil.addActionListener(parent);
-		jbShop.addActionListener(parent);
 	}
 	
 	public void setFileNameBirdPurchase(String username) {
@@ -256,59 +164,7 @@ public class ShopPanel extends JPanel implements ActionListener {
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, areaWidth, areaHeight/12);
-		
-		drawTrophy(g);
-		drawHeart(g);
+		topBar.draw(g);
 	}
-	
-	public void drawTrophy(Graphics g) {
-		g.setColor(new Color(245, 222, 89));
-		g.setFont(new Font("Monospaced", Font.BOLD, 38));
-		FontMetrics metrics = getFontMetrics(g.getFont());
-
-		g.drawImage(resize(biTrophy, areaHeight/12, areaHeight/12), 
-				(areaWidth - areaHeight/12 - 10 - metrics.stringWidth(String.valueOf(trophy.getTrophy())))/2, 
-				0, this);
-
-		g.drawString(String.valueOf(trophy.getTrophy()),
-				(areaWidth - areaHeight/12 - 10 - metrics.stringWidth(String.valueOf(trophy.getTrophy())))/2 + areaHeight/12 + 10, 
-				(areaHeight/12+28)/2);
-	}
-	
-	public void drawHeart(Graphics g) {
-		g.setFont(new Font("Monospaced", Font.BOLD, 38));
-		FontMetrics metrics = getFontMetrics(g.getFont());
-
-		g.drawImage(resize(biHeart, areaHeight/12, areaHeight/12), 
-				3*((areaWidth-3*(areaHeight/9))/4 + areaHeight/9) + 
-				((areaWidth-3*(areaHeight/9))/4 - areaHeight/12 - 10 - metrics.stringWidth(heart.getDisplayTimer()))/2, 
-				0, this);
-
-		g.setColor(Color.WHITE);
-		g.drawString(String.valueOf(heart.getHeart()),
-				3*((areaWidth-3*(areaHeight/9))/4 + areaHeight/9) + 
-				((areaWidth-3*(areaHeight/9))/4 - areaHeight/12 - 10 - metrics.stringWidth(heart.getDisplayTimer()))/2 + 
-				(areaHeight/12 - metrics.stringWidth(String.valueOf(heart.getHeart())))/2, 
-				(areaHeight/12+28)/2);
-
-		g.setColor(new Color(255, 87, 87));
-		g.drawString(heart.getDisplayTimer(),
-				3*((areaWidth-3*(areaHeight/9))/4 + areaHeight/9) + 
-				((areaWidth-3*(areaHeight/9))/4 - areaHeight/12 - 10 - metrics.stringWidth(heart.getDisplayTimer()))/2 +
-				areaHeight/12 + 10, (areaHeight/12+28)/2);
-	}
-	
-	public BufferedImage resize(BufferedImage img, int newW, int newH) { 
-		Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
-		BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-
-		Graphics2D g2d = dimg.createGraphics();
-		g2d.drawImage(tmp, 0, 0, null);
-		g2d.dispose();
-
-		return dimg;
-	} 
 	
 }
