0875cd0df1148cbf88b0c1ed88c5c4bf02355670
auliaaepa <76677130+auliaaepa@users.noreply.github.com>
2021-01-21 23:03:19 +0700

    Update Trophy.java

--- src/com/capadogame/Trophy.java
+++ src/com/capadogame/Trophy.java
@@ -1,12 +1,7 @@
 package com.capadogame;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
+import java.io.*;
+import java.net.*;
 
 public class Trophy {
 	private int trophy;
